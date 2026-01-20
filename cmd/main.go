e3070e558083fdf97c4c5567552eb3574081231e
henrique.rodrigues <atslog@2025>
2026-01-20 17:07:34 -0300

    finalizado o código

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,18 @@
 package main
+
+import (
+	"log"
+	"net/http"
+
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+
+	"github.com/gorilla/mux"
+	"github.com/henriquerrodrigues/go-bookstore/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+	log.Fatal(http.ListenAndServe("localhost:9010", r))
+}
